c653ccb741d1d56a766c0264d8bca9a421ca1e3d
Host-Info.net <50676042+aerobotpro@users.noreply.github.com>
2020-05-11 19:34:19 -0700

    Update HTML_Cloner.py

--- HTML_Cloner.py
+++ HTML_Cloner.py
@@ -1,25 +1,16 @@
-#dr.lixxard
-import requests
-def main():
-    inp=input("\n\nWelcome!\n\nEnter A Url To Pull Response From\n\n---------->>>:")
-    try:
-        r=requests.get(inp)
-        html=r.text
-    except Exception as E:
-        print(str(E))
-        setup()
-    ask=input("[SUCCESS] Would You Like To Save To HTML File?(Y/N):")
-    if "y" or "Y" in ask:
-        print("Writing File...")
-        f=open("Captured_Response.html","w+")
-        f.write(str("""<!--SAVED FROM """+inp+"-->"+"""\n\n-------------------------------------\n\n"""+html))
-        f.close()
-        wait=input("\n\nDONE\n\nPRESS ENTER")
-        main()
-    else:
-        print("""<!--SAVED FROM """+inp+"-->"+"""\n\n-------------------------------------\n\n"""+html)
-        wait=input("\n\nPRESS ENTER")
-        main()
-def setup():
-    main()
-setup()
+from requests_html import HTMLSession
+import webbrowser
+from os import getcwd
+
+site = input("Enter a URL to clone >>>:")
+
+session = HTMLSession()
+r = session.get(site)
+
+r.html.render()
+#print(r.text)
+fileloc = f'{getcwd()}/index.html'
+with open(fileloc, "w+", encoding="utf-8") as f:
+    f.write(r.text)
+
+webbrowser.open("file:///" + fileloc)
